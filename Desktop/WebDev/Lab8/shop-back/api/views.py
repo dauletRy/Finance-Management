a5e47d30ce8d623020aed44d41def1a7fe702547
dauletRy <dauletryskul06@gmail.com>
2026-03-25 18:02:20 +0500

    Lab8

---
+++ Desktop/WebDev/Lab8/shop-back/api/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from .models import Product, Category
+from django.http import JsonResponse
+
+
+# /api/products/
+def products_list(request):
+    products = Product.objects.all()
+    data = []
+
+    for p in products:
+        data.append({
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+            'category_id': p.category_id
+        })
+
+    return JsonResponse(data, safe=False)
+
+
+# /api/products/<id>/
+def product_detail(request, id):
+    try:
+        p = Product.objects.get(id=id)
+        data = {
+            'id': p.id,
+            'name': p.name,
+            'price': p.price,
+            'description': p.description,
+            'count': p.count,
+            'is_active': p.is_active,
+            'category_id': p.category_id
+        }
+        return JsonResponse(data)
+    except Product.DoesNotExist:
+        return JsonResponse({'error': 'Product not found'})
+
+
+# /api/categories/
+def categories_list(request):
+    categories = Category.objects.all()
+    data = [{'id': c.id, 'name': c.name} for c in categories]
+    return JsonResponse(data, safe=False)
+
+
+# /api/categories/<id>/
+def category_detail(request, id):
+    try:
+        c = Category.objects.get(id=id)
+        return JsonResponse({'id': c.id, 'name': c.name})
+    except Category.DoesNotExist:
+        return JsonResponse({'error': 'Category not found'})
+
+
+# /api/categories/<id>/products/
+def category_products(request, id):
+    try:
+        products = Product.objects.filter(category_id=id)
+        data = []
+
+        for p in products:
+            data.append({
+                'id': p.id,
+                'name': p.name,
+                'price': p.price,
+                'description': p.description,
+                'count': p.count,
+                'is_active': p.is_active,
+                'category_id': p.category_id
+            })
+
+        return JsonResponse(data, safe=False)
+    except:
+        return JsonResponse({'error': 'Something went wrong'})
