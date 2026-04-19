b17ee6149845b7ab20f8b9724f96c79b1da436d4
dauletRy <dauletryskul06@gmail.com>
2026-04-19 14:54:22 +0500

    Delete LAB4 directory

--- LAB4/task2/online-store/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductListComponent } from './components/components/product-list/product-list';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: ProductListComponent
-  }
-];
